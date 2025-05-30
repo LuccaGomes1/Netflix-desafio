c05fbaf2368b5e3a08454b900e80636e5bab3af4
Lucca <luccadaveigagomes@gmail.com>
2025-05-30 14:42:47 -0300

    feat: add de js nos posters e animacao nos cartazes

---
+++ js/base.js
@@ -0,0 +1,29 @@
+const spider_man = document.getElementById("spider-man");
+spider_man.addEventListener("click", function () {
+window.open("https://www.youtube.com/watch?v=JfVOs4VSpmA", "_blank");
+});
+
+const shang_chi = document.getElementById("shang-chi");
+shang_chi.addEventListener("click", function () {
+window.open("https://www.youtube.com/watch?v=giWIr7U1deA", "_blank");
+});
+
+const the_witcher = document.getElementById("the-witcher");
+the_witcher.addEventListener("click", function () {
+window.open("https://www.youtube.com/watch?v=tjujvMkqWe4", "_blank");
+});
+
+const wish_dragon = document.getElementById("wish-dragon");
+wish_dragon.addEventListener("click", function () {
+window.open("https://www.youtube.com/watch?v=uWIRyU5fuzU", "_blank");
+});
+
+const loki = document.getElementById("loki");
+loki.addEventListener("click", function () {
+window.open("https://www.youtube.com/watch?v=xdBWrkcmMwU", "_blank");
+});
+
+const batman = document.getElementById("button");
+batman.addEventListener("click", function () {
+window.open("https://www.youtube.com/watch?v=q8ncjHylG6c&t=3s", "_blank");
+});
